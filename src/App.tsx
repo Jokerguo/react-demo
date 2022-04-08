7ab043611cabb9910da5e774508af929bb6f55c1
郭东生 <1977036313@qq.com>
2022-04-08 17:04:50 +0800

    添加编辑标签页

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Money from 'views/Money'
+import Money from 'views/money'
 import NoMatch from 'views/noMatch'
 import Statistics from 'views/statistics'
-import Tags from 'views/tags'
+import Tags from 'views/tag'
+import { EditTag } from 'views/tag/editTag'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/tags" element={<Tags />} />
+        <Route path="/tags/:tag" element={<EditTag />} />
         <Route path="/money" element={<Money />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="*" element={<NoMatch />} />
---
+++ src/views/tag/editTag.tsx
@@ -0,0 +1,5 @@
+const EditTag: React.FC = () => {
+  return <div>详情也</div>
+}
+
+export { EditTag }
